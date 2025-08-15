22051cafc3a7440ccaad116ee387c8c1312cd346
Raman Duggal <rdsaab1@gmail.com>
2025-08-15 20:24:17 +0000

    1/1
    Next.js 15.3.3 (stale)
    Turbopack
    Runtime Error
    
    Error: query is no

--- src/app/sales/customer-entry/customer-entry-client.tsx
+++ src/app/sales/customer-entry/customer-entry-client.tsx
@@ -27,7 +27,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { addCustomer, updateCustomer, deleteCustomer, getCustomers, getCustomersRealtime } from "@/lib/firestore";
-import { DocumentData, QuerySnapshot, onSnapshot, collection, doc } from "firebase/firestore";
+import { DocumentData, QuerySnapshot, onSnapshot, collection, doc, query, orderBy } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 
 import { Pen, PlusCircle, Save, Trash, Info, Settings, Plus, ChevronsUpDown, Check, Calendar as CalendarIcon, User, Phone, Home, Truck, Wheat, Banknote, Landmark, FileText, Hash, Percent, Scale, Weight, Calculator, Building, Milestone, UserSquare, BarChart, Wallet, ChevronRight, Receipt, ArrowRight, LayoutGrid, LayoutList, Rows3, StepForward, X, Server, Hourglass, ClipboardList, FilePlus, InfoIcon, UserCog, PackageSearch, CircleDollarSign } from "lucide-react";
