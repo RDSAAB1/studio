dd3deef8c9b3bb6eb84cd7495639001c70918f4f
Raman Duggal <rdsaab1@gmail.com>
2025-09-02 21:10:23 +0000

    Mere pass iss se bhi strong idea hai setting page pr jo system banaya th

--- src/app/connect-gmail/page.tsx
+++ src/app/connect-gmail/page.tsx
@@ -66,7 +66,7 @@ export default function ConnectGmailPage() {
         try {
             await saveCompanySettings(user.uid, {
                 email: user.email!,
-                appPassword: appPassword,
+                appPassword: appPassword.replace(/\s/g, ''),
             });
             toast({
                 title: "Success!",
