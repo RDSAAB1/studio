171ffe9c96d50be5de4240ef13e10493138e42cf
Raman Duggal <rdsaab1@gmail.com>
2025-09-06 22:22:55 +0000

    Use dynamic import for ExpenseTrackerClient

--- src/app/expense-tracker/page.tsx
+++ src/app/expense-tracker/page.tsx
@@ -1,6 +1,9 @@
-
-import ExpenseTrackerClient from "./expense-tracker-client";
+import dynamic from 'next/dynamic';
 import type { PageProps } from '@/app/types';
+
+const ExpenseTrackerClient = dynamic(() => import('./expense-tracker-client'), {
+  ssr: false,
+});
 
 export default function ExpenseTrackerPage({ searchParams }: PageProps) {
   return (
