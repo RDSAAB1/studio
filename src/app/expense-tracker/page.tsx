f2c963edb5ba0996e8eb6d1a9e804450861abd36
Raman Duggal <rdsaab1@gmail.com>
2025-09-08 07:45:11 +0000

    Pass searchParams to client

--- src/app/expense-tracker/page.tsx
+++ src/app/expense-tracker/page.tsx
@@ -1,8 +1,8 @@
 import DynamicExpenseTrackerClient from './dynamic-expense-tracker';
 import type { PageProps } from '@/app/types';
 
-export default function ExpenseTrackerPage() {
+export default function ExpenseTrackerPage({ searchParams }: PageProps) {
   return (
-    <DynamicExpenseTrackerClient />
+    <DynamicExpenseTrackerClient searchParams={searchParams}/>
   );
 }
