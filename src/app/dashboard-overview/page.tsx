bff294d0133ccb9ba6fb54be6a2dd01dc21b4c4a
Raman Duggal <rdsaab1@gmail.com>
2025-09-16 08:52:29 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/dashboard-overview/page.tsx
+++ src/app/dashboard-overview/page.tsx
@@ -1,7 +1,6 @@
 
 import DashboardOverviewClient from "./dashboard-overview-client";
 import type { PageProps } from '@/app/types';
-import { Tremor, Card, Text, Metric } from "@tremor/react";
 
 export default function DashboardOverviewPage({ params, searchParams }: PageProps) {
   return <DashboardOverviewClient />;
